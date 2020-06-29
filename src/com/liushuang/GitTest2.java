41c8d9655bf7acd8d690a9542aa1023629cb4241
waijl <65709787+waijl@users.noreply.github.com>
2020-06-29 22:47:37 +0800

    Update GitTest2.java

--- src/com/liushuang/GitTest2.java
+++ src/com/liushuang/GitTest2.java
@@ -5,7 +5,11 @@ package com.liushuang;
  * @create 2020-06-29 22:26
  */
 public class GitTest2 {
-    public void test(){
+    public void test1(){
         System.out.println("hello world!!!!");
+    }
+
+    public void test2(){
+        System.out.println("github上面更新");
     }
 }
